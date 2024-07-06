b4409e4b62c519382b96ee89318359bca4332b6b
Jose Severiche <severichejose90@gmail.com>
2024-07-06 12:04:56 -0300

    se modifico archivo mysql.config.js

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -1,8 +1,15 @@
-const config = {
+/*const config = {
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'shakira_app'
+    database: 'shakira_db'
+}*/
+
+const config = {
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.DB
 }
 
 export default config;
--- server/server.js
+++ server/server.js
@@ -29,9 +29,6 @@ export default class Server {
         Server.runServer(port);
     }
 
-
-
-
 }
 
 
